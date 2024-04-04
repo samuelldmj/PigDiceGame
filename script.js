043b54e4e0b4ceb12b73a5ca22ada8a72a61c4d5
Samuel <samuelldmj5@gmail.com>
2024-04-04 20:25:13 +0100

    hold button and dice rolling buttons now mworks

--- script.js
+++ script.js
@@ -28,43 +28,82 @@ let currentScore = 0;
 let activePlayer = 0;
 
 //active player pane
-const player0element = document.querySelector('.player--0')
-const player1element = document.querySelector('.player--1')
+const player0element = document.querySelector('.player--0');
+const player1element = document.querySelector('.player--1');
+
+//Holding the big score
+const scores = [0, 0];
+
+//checking if playing is on
+let playing = true;
+
+//switching between player function,  so as to maintain DRY
+let switchPlayer = function () {
+
+    //making the current score of the active player 0, after hitting one on the dice
+    document.querySelector(`#current--${activePlayer}`).textContent = 0;
+    //it helps holding the score of the active player to 0.
+    currentScore = 0;
+    //switching to the next player
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    //changes the background to the active player
+    player0element.classList.toggle('player--active');
+    player1element.classList.toggle('player--active');
+
+};
+
 
 //making the dice roll on clicking using the event listner
 btnRoll.addEventListener('click', function () {
+    if (playing) {
 
-    //unhiding the dice image when the rolling button is clicked
-    diceElement.classList.remove('hidden');
+        //unhiding the dice image when the rolling button is clicked
+        diceElement.classList.remove('hidden');
 
-    //generating random numbers from 1 to 6 and appending it to the dice images
-    let dice = Math.trunc(Math.random() * 6) + 1;
+        //generating random numbers from 1 to 6 and appending it to the dice images
+        let dice = Math.trunc(Math.random() * 6) + 1;
 
-    //This make the image change dynamically
-    diceElement.src = `dice-${dice}.png`;
+        //This make the image change dynamically
+        diceElement.src = `dice-${dice}.png`;
 
 
-    if (dice !== 1) {
-        //incrementing the current score as dice is rolled
-        currentScore += dice;
+        if (dice !== 1) {
+            //incrementing the current score as dice is rolled
+            currentScore += dice;
 
-        //dynamically displaying the current score of the active player
-        document.querySelector(`#current--${activePlayer}`).textContent = currentScore;
+            //dynamically displaying the current score of the active player
+            document.querySelector(`#current--${activePlayer}`).textContent = currentScore;
 
-    } else {
-        //switching to the next player
-        document.querySelector(`#current--${activePlayer}`).textContent = 0;
-        currentScore = 0;
-        activePlayer = activePlayer === 0 ? 1 : 0;
-        player0element.classList.toggle('player--active')
-        player1element.classList.toggle('player--active')
+        } else {
+            //switch player
+            switchPlayer();
+
+        };
 
     }
-
-
-
 })
 
+
+btnhold.addEventListener('click', function () {
+    if (playing) {
+
+        //add current score to the big score
+        scores[activePlayer] += currentScore;
+
+        document.querySelector(`#score--${activePlayer}`).textContent = scores[activePlayer];
+
+        if (scores[activePlayer] >= 20) {
+            playing = false;
+            document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+            document.querySelector(`.player--${activePlayer}`).classList.add('player--active');
+            diceElement.classList.add('hidden');
+        } else {
+            //switch player
+            switchPlayer();
+        }
+    }
+
+})
 
 
 
